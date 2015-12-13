4dfb7a4bf0395a4b32d2a535b61ab33ccf41df74
Alberto Revel <alberto.revel@gmail.com>
2015-12-14 00:00:38 +0100

    Lab5 done

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -32,17 +32,29 @@ public class SearchController {
 
         Map<String,Object> params = new HashMap<>();
 
+        // We add all the query less the last word unless there's only one.
         String search = args[0];
-        for(int i = 1; i < args.length -1; i++) {
+        int lastIdx = args.length - 1;
+        for(int i = 1; i < lastIdx; i++) {
             search += " " + args[i];
+        }
+
+        // If the last word is another normal word to search, we add it to the
+        // query. If not, we set the info for the max param.
+        if(lastIdx != 0) {
+            if(args[lastIdx].startsWith("max:")) {
+                String[] max = args[lastIdx].split(":");
+                if(max[0].equals("max")) {
+                    params.put("CamelTwitterCount", max[1]);
+                }
+            } else {
+                search += " " + args[lastIdx];
+            }
         }
 
         params.put("CamelTwitterKeywords",search);
 
-        String[] max = args[1].split(":");
-        if(max[0].equals("max")) {
-            params.put("CamelTwitterCount", max[1]);
-        }
+
         return producerTemplate.requestBodyAndHeaders("direct:search", "", params);
     }
 }
